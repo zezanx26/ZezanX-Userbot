f4d0560325b2e8cfacb6d64dd14a7ef271b3ca7c
zezanx26 <102902672+zezanx26@users.noreply.github.com>
2022-04-18 09:34:25 +0700

    Update and rename userbot/modules/ping.py to bot pengguna/modul/ping.py

--- bot pengguna/modul/ping.py
+++ bot pengguna/modul/ping.py
@@ -60,12 +60,12 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@register(incoming=True, from_users=1663258664, pattern=r"^.absen$")
+@register(incoming=True, from_users=1863783304, pattern=r"^.absen$")
 async def _(kyy):
     await kyy.reply(random.choice(absen))
 
 
-@register(incoming=True, from_users=1663258664, pattern=r"^.kyy$")
+@register(incoming=True, from_users=1863783304, pattern=r"^.zezan$")
 async def _(kyy):
     await kyy.reply(random.choice(pacar))
 
