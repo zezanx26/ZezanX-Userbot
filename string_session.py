c0a50217266a733e518fcf8e8bae8bc7cd0f6a78
zezanx26 <102902672+zezanx26@users.noreply.github.com>
2022-04-18 07:59:03 +0700

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -19,7 +19,7 @@ API_HASH = input("Enter API_HASH here: ")
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     print("Check Telegram Save Message Mu Untuk Copy STRING_SESSION ")
     session_string = client.session.save()
-    saved_messages_template = """😼 Thank u for Support ZezanX-Userbot | Channel: @ahhsudahlahhh
+    saved_messages_template = """😼 Thank u for Support ZezanX-Userbot | Channel: @zezanxproject
 <code>STRING_SESSION</code>: <code>{}</code>
 ⚠️ <i>Please be careful before passing this value to third parties</i>""".format(session_string)
     client.send_message("me", saved_messages_template, parse_mode="html")
